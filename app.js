80a58ff79a86023ca3c855318a39a42a3f3afe96
Ayatallah <ayatallah.amr@gmail.com>
2017-06-28 18:12:43 +0200

    working app with ui routing, with #

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+	'use strict';
+	angular.module('TaskApp',['ui.router'])
+	.config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider){
+		$stateProvider
+    	.state('home', {
+        url: "/home",
+        templateUrl: 'app/partials/home.html'
+    });
+    
+    // The following couple of lines are used to remove # for more pretty URLs
+   // Will need node/express rewrites to work with refresh
+ 	/*    
+	$locationProvider.html5Mode({
+     	enabled: true,
+     	requireBase: false
+ 	});
+ 	*/
+	}]);
+}());
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var connect = require('gulp-connect');
 //Connect Taks
 gulp.task('connect', function () {
   connect.server({
-    root: 'app/',
     port: 8888
   });
 });
