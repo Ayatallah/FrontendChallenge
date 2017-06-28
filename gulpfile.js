c904c0a81282abda123e444dfe4d2955a7680ef0
Ayatallah <ayatallah.amr@gmail.com>
2017-06-29 01:35:06 +0200

    task to compile sass done

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
 
 //Connect Taks
 gulp.task('connect', function () {
   connect.server({
     port: 8888
   });
+});
+
+//Sass and Foundation Task
+gulp.task('style', function(){
+  return gulp.src('app.scss')
+    .pipe(sass({style: 'expanded'}))
+    .pipe(gulp.dest('./style'));
 });
