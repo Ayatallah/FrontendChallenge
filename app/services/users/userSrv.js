b761a0ea84cd65238507fe6c45291a19c9b72b60
Ayatallah <ayatallah.amr@gmail.com>
2017-06-30 00:32:35 +0200

    updated user servic, now test passes

--- app/services/users/userSrv.js
+++ app/services/users/userSrv.js
@@ -1,8 +1,15 @@
-angular.module('TaskApp').factory('userSrv',userSrv);
-function userSrv() {
+angular.module('TaskApp').factory('userSrv',['$http', userSrv]);
+
+function userSrv($http) {
     var userSrv = {};
     userSrv.getUsers = function(){
-
+    	
+    	return $http.get('https://api.github.com/users').then(function(result){
+    		if(result.status == 200){
+    			return result.data;
+    		}
+    	});
+    	
 	};
 	return userSrv;
 
