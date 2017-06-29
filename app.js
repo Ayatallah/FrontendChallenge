4057dad1549e87988d54dc0da909e3f8a0335db1
Ayatallah <ayatallah.amr@gmail.com>
2017-06-29 23:41:21 +0200

    basic routes added

--- app.js
+++ app.js
@@ -5,11 +5,25 @@
 		$stateProvider
 
     	.state('home', {
-	        url: "/home",
-	        templateUrl: 'app/partials/home.html'
+	        url: "/",
+	        templateUrl: 'app/partials/home/home.html'
+    	})
+    	.state('about', {
+	        url: "/about",
+	        templateUrl: 'app/partials/home/home.html'
+    	})
+        .state('users', {
+	        url: "/users",
+	        templateUrl: 'app/partials/user/users.html'
+    	})
+    	.state('user', {
+	        url: "/users/:login",
+	        templateUrl: 'app/partials/user/users.html'
     	});
-    	
-    
+    	$urlRouterProvider.when('/home', '/');
+    	$urlRouterProvider.when('/about/', '/');
+    	$urlRouterProvider.otherwise('/');
+
     // The following couple of lines are used to remove # for more pretty URLs
    // Will need node/express rewrites to work with refresh
  	/*    
