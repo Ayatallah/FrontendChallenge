b4d097407864aec6a77ef10476b23ede39dc627d
Ayatallah <ayatallah.amr@gmail.com>
2017-06-30 20:27:26 +0200

    Added dynamic title

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 (function(){
 	'use strict';
-	angular.module('TaskApp',['ui.router'])
+	angular.module('TaskApp',['ui.router','ui.router.title'])
 	.config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider){
 		$stateProvider
 
     	.state('home', {
 	        url: "/",
-	        templateUrl: 'app/partials/home/home.html'
+	        templateUrl: 'app/partials/home/home.html',
+            resolve: {
+                $title: function() { return 'Home'; }
+            }
     	})
     	.state('about', {
 	        url: "/about",
-	        templateUrl: 'app/partials/home/home.html'
+	        templateUrl: 'app/partials/home/home.html',
+            resolve: {
+                $title: function() { return 'About'; }
+            }
     	})
         .state('users', {
 	        url: "/users",
-	        templateUrl: 'app/partials/user/users.html'
+	        templateUrl: 'app/partials/user/users.html',
+            resolve: {
+                $title: function() { return 'Users'; }
+            }
     	})
     	.state('user', {
 	        url: "/users/:login",
-	        templateUrl: 'app/partials/user/users.html'
+	        templateUrl: 'app/partials/user/users.html',
+            resolve: {
+                $title: function() { return 'Users'; }
+            }
     	});
     	$urlRouterProvider.when('/home', '/');
     	$urlRouterProvider.when('/about/', '/');
