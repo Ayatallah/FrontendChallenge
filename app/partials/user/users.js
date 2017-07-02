ea2cdf77a37e047010c647deffb15a033e70e564
Ayatallah <ayatallah.amr@gmail.com>
2017-07-03 00:17:21 +0200

    users controller tests done

--- app/partials/user/users.js
+++ app/partials/user/users.js
@@ -7,13 +7,14 @@ function UsersCtrl($rootScope,$scope,$state, userSrv,usersNeeded){
 	userSrv.getUser(vm.users[0].login).then(function (user) {
 			vm.user=user;
 	});
-	$rootScope.$on('$stateChangeSuccess', 
-	function(){ 
+	
+	vm.getuser = function(){ 
 		//console.log($state.params.login);
 		userSrv.getUser($state.params.login).then(function (user) {
 			vm.user=user;
 		});
-	});
+	}
+	$rootScope.$on('$stateChangeSuccess', vm.getuser);
 	vm.loadmore = function(){
 		userSrv.getUsers().then(function (users) {
 			vm.users= (vm.users).concat(users);
--- app/partials/user/users.spec.js
+++ app/partials/user/users.spec.js
@@ -1,12 +1,27 @@
 describe('UsersCtrl', function(){
-	var ctrl;
+	var ctrl,$scope, mockUserSrv;
 
 	beforeEach(angular.mock.module('TaskApp'));
 	
-	beforeEach(inject(function($controller) {
-		ctrl = $controller('UsersCtrl');
+	beforeEach(inject(function($controller, $rootScope, userSrv, $state, $q) {
+		$scope = $rootScope.$new();
+		mockUserSrv = userSrv;
+		deferred = $q.defer();
+		spyOn(mockUserSrv,"getUser").and.returnValue(deferred.promise);
+		spyOn(mockUserSrv,"getUsers").and.returnValue(deferred.promise);
+		var users = [{'login':1},{'login':2}];
+		ctrl = $controller("UsersCtrl", {$scope: $scope, userSrv: mockUserSrv, usersNeeded:users});
 	}));
 
-	// it('', function(){ 
- //    });
+    it("Test UsersCtrl.getUser", function () {
+        var login = 1;
+        ctrl.getuser(login);
+        expect(mockUserSrv.getUser).toHaveBeenCalled();
+	});
+
+	it("Test UsersCtrl.loadmore", function () {
+        ctrl.loadmore();
+        expect(mockUserSrv.getUsers).toHaveBeenCalled();
+	});
+
 })
